eae8a0b32866e2b64821c7681582d16add933d3d
yoichiro <yoichiro@eisbahn.jp>
2011-07-01 09:04:41 +0900

    Re-factoring MixiWrapper interface.

---
+++ src/jp/eisbahn/android/sdk/wrapper/MixiWrapperImplTest.java
@@ -0,0 +1,20 @@
+package jp.eisbahn.android.sdk.wrapper;
+
+import jp.eisbahn.android.sdk.wrapper.people.PeopleProxyImpl;
+import jp.eisbahn.android.sdk.wrapper.photo.PhotoProxyImpl;
+import jp.eisbahn.android.sdk.wrapper.request.RequestProxyImpl;
+import jp.mixi.android.sdk.MixiContainer;
+
+import org.easymock.EasyMock;
+
+public class MixiWrapperImplTest extends AbstractTest {
+
+    public void testSimple() throws Exception {
+        MixiContainer container = EasyMock.createMock(MixiContainer.class);
+        MixiWrapperImpl target = new MixiWrapperImpl(container);
+        assertTrue(target.getPeopleAPI() instanceof PeopleProxyImpl);
+        assertTrue(target.getRequestAPI() instanceof RequestProxyImpl);
+        assertTrue(target.getPhotoAPI() instanceof PhotoProxyImpl);
+    }
+    
+}
