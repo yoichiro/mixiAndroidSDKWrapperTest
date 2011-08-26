226e4d59d906e8b773667155f7375a4ef37f84a6
yoichiro <yoichiro@eisbahn.jp>
2011-08-26 09:15:45 +0900

    Completed to support all Voice API.

--- src/jp/eisbahn/android/sdk/wrapper/voice/VoiceProxyImplTest.java
+++ src/jp/eisbahn/android/sdk/wrapper/voice/VoiceProxyImplTest.java
@@ -1,9 +1,13 @@
 package jp.eisbahn.android.sdk.wrapper.voice;
 
+import java.io.ByteArrayInputStream;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 import jp.eisbahn.android.sdk.wrapper.AbstractTest;
+import jp.eisbahn.android.sdk.wrapper.CallbackAdapter;
+import jp.mixi.android.sdk.HttpMethod;
 import jp.mixi.android.sdk.MixiContainer;
 import android.test.mock.MockContext;
 
@@ -269,6 +273,119 @@ public class VoiceProxyImplTest extends AbstractTest {
         params.setCount(456);
         params.setTrimUser(TrimUser.ID_AND_SCREEN_NAME);
         target.getStatusFavorites(postId, params, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testPostStatus() throws Exception {
+        GetStatusesCallbackHandler handler = new GetStatusesCallbackHandler(new MockContext());
+        String status = "つぶやき1";
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("status", URLEncoder.encode(status, "UTF-8"));
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/statuses", HttpMethod.POST, map, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.postStatus(status, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testPostStatusWithImage() throws Exception {
+        GetStatusesCallbackHandler handler = new GetStatusesCallbackHandler(new MockContext());
+        String status = "つぶやき1";
+        byte[] image = new byte[] {1, 2, 3, 4, 5};
+        ByteArrayInputStream in = new ByteArrayInputStream(image);
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/statuses?status=" + URLEncoder.encode(status, "UTF-8"),
+                "image/jpeg", in, image.length, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.postStatus(status, in, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testDeleteStatus() throws Exception {
+        CallbackAdapter handler = new CallbackAdapter(new MockContext());
+        String postId = "postId1";
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/statuses/" + postId,
+                HttpMethod.DELETE, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.deleteStatus(postId, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testPostStatusComment() throws Exception {
+        CallbackAdapter handler = new CallbackAdapter(new MockContext());
+        String text = "こめんと1";
+        String postId = "postId1";
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("text", URLEncoder.encode(text, "UTF-8"));
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/replies/" + postId,
+                HttpMethod.POST, map, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.postStatusComment(postId, text, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testDeleteStatusComment() throws Exception {
+        CallbackAdapter handler = new CallbackAdapter(new MockContext());
+        String postId = "postId1";
+        String commentId = "commentId";
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/replies/" + postId + "/" + commentId,
+                HttpMethod.DELETE, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.deleteStatusComment(postId, commentId, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testPostStatusFavorite() throws Exception {
+        CallbackAdapter handler = new CallbackAdapter(new MockContext());
+        String postId = "postId1";
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/favorites/" + postId,
+                HttpMethod.POST, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.postStatusFavorite(postId, handler);
+        
+        AndroidMock.verify(mixiContainer);
+    }
+
+    public void testDeleteStatusFavorite() throws Exception {
+        CallbackAdapter handler = new CallbackAdapter(new MockContext());
+        String postId = "postId1";
+        String userId = "userId";
+        
+        MixiContainer mixiContainer = AndroidMock.createMock(MixiContainer.class);
+        mixiContainer.send("/voice/favorites/" + postId + "/" + userId,
+                HttpMethod.DELETE, handler);
+        AndroidMock.replay(mixiContainer);
+        
+        VoiceProxyImpl target = new VoiceProxyImpl(mixiContainer);
+        target.deleteStatusFavorite(postId, userId, handler);
         
         AndroidMock.verify(mixiContainer);
     }
