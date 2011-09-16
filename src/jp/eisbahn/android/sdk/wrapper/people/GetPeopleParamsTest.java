0bc473f59ae5ca220b8f7089bafa8c7f7e294c58
yoichiro <yoichiro@eisbahn.jp>
2011-09-16 20:28:14 +0900

    Adding the count parameter to the parameter class to fetch the people.

--- src/jp/eisbahn/android/sdk/wrapper/people/GetPeopleParamsTest.java
+++ src/jp/eisbahn/android/sdk/wrapper/people/GetPeopleParamsTest.java
@@ -13,8 +13,10 @@ public class GetPeopleParamsTest extends AbstractTest {
 
     public void testConvertParameterMapFull() throws Exception {
         int startIndex = 1;
+        int count = 2;
         GetPeopleParams target = new GetPeopleParams();
         target.setStartIndex(startIndex);
+        target.setCount(count);
         target.setSortBy(SortBy.displayName);
         target.setSortOrder(SortOrder.descending);
         target.setThumbnailPrivacy(ThumbnailPrivacy.friends);
@@ -25,6 +27,7 @@ public class GetPeopleParamsTest extends AbstractTest {
         });
         Map<String, String> actual = target.convertParameterMap();
         assertEquals("1", actual.get("startIndex"));
+        assertEquals("2", actual.get("count"));
         assertEquals("displayName", actual.get("sortBy"));
         assertEquals("descending", actual.get("sortOrder"));
         assertEquals("friends", actual.get("thumbnailPrivacy"));
